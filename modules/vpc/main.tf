f7e7a358e57851b9c667bd3b2290a3f81fc4e7f5
KobeMadeTech <139927225+KobeCyber@users.noreply.github.com>
2025-09-01 13:04:53 -0500

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,1 +1,38 @@
+resource "aws_vpc" "this" {
+cidr_block = var.cidr_block
+enable_dns_hostnames = true
+enable_dns_support = true
+tags = { Name = "${var.name}-vpc" }
+}
 
+
+resource "aws_internet_gateway" "igw" {
+vpc_id = aws_vpc.this.id
+tags = { Name = "${var.name}-igw" }
+}
+
+
+resource "aws_subnet" "public" {
+for_each = toset(var.public_subnet_cidrs)
+vpc_id = aws_vpc.this.id
+cidr_block = each.value
+map_public_ip_on_launch = true
+tags = { Name = "${var.name}-public-${replace(each.value, "/", "-")}" }
+}
+
+
+resource "aws_route_table" "public" {
+vpc_id = aws_vpc.this.id
+route {
+cidr_block = "0.0.0.0/0"
+gateway_id = aws_internet_gateway.igw.id
+}
+tags = { Name = "${var.name}-public-rt" }
+}
+
+
+resource "aws_route_table_association" "public" {
+for_each = aws_subnet.public
+subnet_id = each.value.id
+route_table_id = aws_route_table.public.id
+}
